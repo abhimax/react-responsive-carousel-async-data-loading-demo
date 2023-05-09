5e0191034770875d7c45672a7085a456e3e3e0d3
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-05-10 01:36:57 +0530

    feat: Add Fetch Album Chunk Async Carousel

---
+++ src/components/FetchAlbumChunkAsyncCarousel.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { Carousel } from "react-responsive-carousel";
+
+const FetchAlbumChunkAsyncCarousel = () => {
+  const [slideData, setSlideData] = useState([]);
+  const [slideStatus, setSlideStatus] = useState({
+    currentItem: 0,
+    total: 0,
+  });
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const fetchSlideData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/albums/1/photos"
+      );
+      const data = await response.json();
+      setSlideData(
+        data.map((item) => ({ image: item.url, caption: item.title }))
+      );
+    };
+    fetchSlideData();
+  }, []);
+
+  const chunkedSlideData = Array.from(
+    { length: Math.ceil(slideData.length / 8) },
+    (_, index) => {
+      const startIndex = index * 8;
+      return slideData.slice(startIndex, startIndex + 8);
+    }
+  );
+
+  return (
+    <>
+      <h2>Fetch Album Chunk Async Carousel</h2>
+      <Carousel
+        statusFormatter={(currentItem, total) => {
+          console.log("currentItem", currentItem, "total", total);
+          setCurrentSlide(currentItem);
+        }}
+        showThumbs={false}
+      >
+        {chunkedSlideData.map((slideData, index) => {
+          console.log(">>>>>>", index);
+          if (index + 1 !== currentSlide) {
+            return (
+              <div className="slide-wrapper">{`Loading Slide ${
+                index + 1
+              } Data ...`}</div>
+            );
+          } else {
+            return (
+              <div className="slide-wrapper" key={Math.random()}>
+                {slideData.map((image) => {
+                  return (
+                    <div key={Math.random()} className="slide-tile">
+                      <img
+                        className="slide-image"
+                        src={image.image}
+                        alt={image.image}
+                      />
+                      <p className="image-des">{image.caption}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }
+        })}
+      </Carousel>
+      <p>
+        Current slide: <span className="special-text">{currentSlide}</span>
+      </p>
+    </>
+  );
+};
+
+export default FetchAlbumChunkAsyncCarousel;
